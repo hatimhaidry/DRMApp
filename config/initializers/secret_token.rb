7944ca07a959a666b5b734a38a716f0afe2ff4c0
hatimhaidry <haidryhatim@indiatimes.com>
2012-10-01 18:15:16 +0530

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DRMApp::Application.config.secret_token = 'cd75a98992944456ee9e21764bb610d3c2b43d29ec06a8b2986bd3e8ade594c8425285f52014182ce19361b96111202f69791f902c1f2692a5bebb8bf6b0a452'
